31cf02562b301725a0b4c6484b08109525f0196c
agc en Supernova <agalveca@gmail.com>
2013-11-13 17:58:08 +0100

    SN añadidas carpetas en aprendizaje y en node

---
+++ aprendizaje/angular_casts/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularCasts::Application.config.secret_key_base = '07cb4b3eedb069b05037d2ec7d533b7ac015c9d098243e2b320c3c45ae78cdf73cacc16a1fc0c5609902a7e1835bf5447d5048cda8119f198befb1fcdcd1c8ee'
