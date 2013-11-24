19c252f9b1d7c5f7fb00655482ee4881a4cd2360
agc en Nova <agc@pnte.cfnavarra.es>
2013-11-24 13:00:35 +0100

    N ejemplo rails

---
+++ aprendizaje/rails/league-tutorial-rails4/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LeagueTutorialRails4::Application.config.secret_key_base = '5ce7f126f3a49cd325acfdd82ad9c2b953fe85ea2b39bc7d8841db8bc994270e8cc2f48f6c9f164cbda8eedf303e4e0b31d24ce4b0dc2d8b33dea86ef40d753e'
